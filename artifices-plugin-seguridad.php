e87315fbe52774b556106e7c6230efcb61911c90
Jorge Cortés <jcortesanguita@gmail.com>
2017-12-28 19:07:38 +0100

    refactor: code clean up

--- artifices-plugin-seguridad.php
+++ artifices-plugin-seguridad.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Opciones de Seguridad para clientes Artifices
 Plugin URI: https://www.artifices.net
-Description: Opciones básicas de seguridad: límite de revisiones, no actualizaciones automaticas, no edición de ficheros, no instalación de plugins. 
+Description: Opciones básicas de seguridad: límite de revisiones, no actualizaciones automaticas, no edición de ficheros, no instalación de plugins.
 * Version: 1.0.4
 * Author: Jesus Cortes
 * Author URI: http://www.artifices.net
@@ -10,33 +10,33 @@ License: GPLv2 or later
 Text Domain: artifices
 */
 
-if( ! class_exists( 'Artifices_Updater' ) ){
-    include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+if(!class_exists('Artifices_Updater')){
+    include_once(plugin_dir_path(__FILE__) . 'updater.php');
 }
 
-$updater = new Artifices_Updater( __FILE__ );
-$updater->set_username( 'artifices' );
-$updater->set_repository( 'artifices-seguridad-plugin' );
+$updater = new Artifices_Updater(__FILE__);
+$updater->set_username('artifices');
+$updater->set_repository('artifices-seguridad-plugin');
 /*
-    $updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+    $updater->authorize('abcdefghijk1234567890'); // Your auth code goes here for private repos
 */
 $updater->initialize();
 
 // PERSONALIZAR LOGO LOGIN
-
-function my_login_logo() { ?>
+function my_login_logo_style() { ?>
     <style type="text/css">
-        #login h1 a, .login h1 a {
-            background-image: url(https://www.artifices.net/wp-content/uploads/2017/03/logo_web_01.png);
-		height:52px;
-		width:240px;
-		background-size: 240px 52px;
-		background-repeat: no-repeat;
-        	padding-bottom: 30px;
+        #login h1 a,
+        .login h1 a {
+            background-image: url("https://www.artifices.net/wp-content/uploads/2017/03/logo_web_01.png");
+            background-size: 240px 52px;
+            background-repeat: no-repeat;
+            height: 52px;
+            width: 240px;
+            padding-bottom: 30px;
         }
     </style>
 <?php }
-add_action( 'login_enqueue_scripts', 'my_login_logo' );
+add_action('login_enqueue_scripts', 'my_login_logo_style');
 
 function define_constants() {
 

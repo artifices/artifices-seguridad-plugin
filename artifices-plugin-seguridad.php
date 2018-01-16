7abe345b579b1a7b55b04057c9bbfcf10a5f61f5
Artifices.net <info@artifices.net>
2018-01-16 11:56:05 +0100

    Cambio en el logo de login, link a la web de Artifices

--- artifices-plugin-seguridad.php
+++ artifices-plugin-seguridad.php
@@ -3,7 +3,7 @@
 Plugin Name: Opciones de Seguridad para clientes Artifices
 Plugin URI: https://www.artifices.net
 Description: Opciones básicas de seguridad: límite de revisiones, no actualizaciones automaticas, no edición de ficheros, no instalación de plugins. 
-* Version: 1.0.4
+* Version: 1.0.5
 * Author: Jesus Cortes
 * Author URI: http://www.artifices.net
 License: GPLv2 or later
@@ -45,7 +45,7 @@ define('DISALLOW_FILE_MODS',true);
 function my_login_logo() { ?>
     <style type="text/css">
         #login h1 a, .login h1 a {
-            background-image: url('<?php echo plugins_url( 'logo-artifices.png', __FILE__ ) ; ?>');
+            background-image: url('<?php echo plugins_url( 'logo-artifices.png', __FILE__ ) ; ?>') !important;
 		height:52px;
 		width:240px;
 		background-size: 240px 52px;
